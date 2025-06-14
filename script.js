a69ed9d7921d36702fc9f1417debb38d6d7e340b
Eduardo Rodrigues <eduardorodrigues@gmail.com>
2025-06-14 13:41:51 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,187 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Produtos com categorias e descrições
-  const produtos = [
-    {
-      id: 1,
-      nome: "Camiseta Estampada",
-      categoria: "roupas",
-      precoAntigo: 79.90,
-      precoNovo: 59.90,
-      img: "assets/images/camisa2.png",
-      descricao: "Camiseta 100% algodão com estampa moderna e confortável."
-    },
-    {
-      id: 2,
-      nome: "Camiseta Estampada",
-      categoria: "roupas",
-      precoAntigo: 79.90,
-      precoNovo: 59.90,
-      img: "assets/images/camisa1.png",
-      descricao: "Camiseta casual com design exclusivo e tecido respirável."
-    },
-    {
-      id: 3,
-      nome: "Fone Bluetooth",
-      categoria: "acessorios",
-      precoAntigo: 299.00,
-      precoNovo: 249.00,
-      img: "assets/images/fone1.webp",
-      descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas."
-    },
-    {
-      id: 4,
-      nome: "iPhone 14 Pro",
-      categoria: "celulares",
-      precoAntigo: 7999.99,
-      precoNovo: 5399.99,
-      img: "assets/images/iph14pro2.webp",
-      descricao: "Apple iPhone 14 Pro Max 256GB Prateado 6,7” 48MP"
-    },
-    {
-      id: 5,
-      nome: "iPhone 16 Pro",
-      categoria: "celulares",
-      precoAntigo: 7999.99,
-      precoNovo: 7399.99,
-      img: "assets/images/iph16pro.webp",
-      descricao: "iPhone 16 Pro Max Apple 256GB, Câmera Tripla de 48MP, Tela 6,9, Titânio-Deserto"
-    },
-    {
-      id: 6,
-      nome: "PC Gamer",
-      categoria: "informatica",
-      precoAntigo: 2399.90,
-      precoNovo: 1599.90,
-      img: "assets/images/pc4.webp",
-      descricao: "Completo Intel Core i5 SSD 480GB 16GB Teclado Mouse Mouse Pad e Headset Gamer Monitor 19 Windows 10 Pro "
-    },
-    {
-    id: 7,
-      nome: "PC Gamer",
-      categoria: "informatica",
-      precoAntigo: 2699.90,
-      precoNovo: 1899.90,
-      img: "assets/images/pc5.webp",
-      descricao: "PC Gamer Completo 3green Play Intel Core i7 16GB RAM Placa de vídeo Geforce 4GB SSD 480GB Monitor 20 75Hz Fonte 500W 3GP-056 "
-    },
-    {
-    id: 8,
-      nome: "PC Gamer",
-      categoria: "informatica",
-      precoAntigo: 3699.90,
-      precoNovo: 2899.90,
-      img: "assets/images/pc6.webp",
-      descricao: "PC Gamer AMD Ryzen 5 4600G, 16GB RAM, Placa de video Radeon Vega 7, SSD 480GB, Fonte 550W + Monitor 23 LED 75hz + Kit Gamer Completo "
-    },
-    {
-    id: 9,
-      nome: "PC Gamer",
-      categoria: "informatica",
-      precoAntigo: 3699.90,
-      precoNovo: 2899.90,
-      img: "assets/images/pc7.webp",
-      descricao: "PC Gamer Completo AMD Ryzen 7 5700G, Gráficos Radeon VEGA 8, 16GB DDR4, SSD 512GB, Fonte 500W, Monitor 21.5 75Hz, 3green Force - 3F-016 "
-    },
-    {
-    id: 10,
-      nome: "PC Gamer",
-      categoria: "informatica",
-      precoAntigo: 4699.90,
-      precoNovo: 3899.90,
-      img: "assets/images/pc8.webp",
-      descricao: "PC Gamer Completo Skill Ryzen 7 5700G, 16GB, Radeon Vega 8, SSD 512GB M.2, + Monitor 27 IPS 100Hz - SK27003 "
-    },
-    {
-      id: 11,
-      nome: "iPhone 16 ",
-      categoria: "celulares",
-      precoAntigo: 7999.99,
-      precoNovo: 6399.99,
-      img: "assets/images/iph16.webp",
-      descricao: "Apple iPhone 16 128GB Rosa 6,1 48MP iOS 5G"
-    },
-     {
-      id: 12,
-      nome: "iPhone 15 ",
-      categoria: "celulares",
-      precoAntigo: 7999.99,
-      precoNovo: 5399.99,
-      img: "assets/images/iph15.webp",
-      descricao: "Apple iPhone 15 de 256GB - Preto"
-    },
-    {
-      id: 13,
-      nome: "Camiseta Estampada",
-      categoria: "roupas",
-      precoAntigo: 79.90,
-      precoNovo: 59.90,
-      img: "assets/images/camisa3.webp",
-      descricao: "Camisa Social Masculina Manga Longa Lisa Slim - LORD FINESSE OUTLET."
-    },
-    {
-      id: 14,
-      nome: "Camiseta Estampada",
-      categoria: "roupas",
-      precoAntigo: 79.90,
-      precoNovo: 59.90,
-      img: "assets/images/camisa4.webp",
-      descricao: "Camisa Social Masculina Moda Maior Homens Plus Size - LORD FINESSE CAMISARIA"
-    },
-    {
-      id: 15,
-      nome: "Camiseta do Campeão",
-      categoria: "roupas",
-      precoAntigo: 129.90,
-      precoNovo: 99.90,
-      img: "assets/images/camisa5.webp",
-      descricao: "Camisa Grêmio I 21/22 Umbro Masculina - Azul+Branco"
-    },
-    {
-      id: 16,
-      nome: "Camiseta do Campeão",
-      categoria: "roupas",
-      precoAntigo: 129.90,
-      precoNovo: 99.90,
-      img: "assets/images/camisa6.webp",
-      descricao: "Camisa Grêmio I 25/26 Torcedor Umbro Feminina"
-    },
-    {
-      id: 17,
-      nome: "Fone Bluetooth",
-      categoria: "acessorios",
-      precoAntigo: 299.00,
-      precoNovo: 249.00,
-      img: "assets/images/fone2.webp",
-      descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas."
-    },
-    {
-      id: 18,
-      nome: "Fone Bluetooth",
-      categoria: "acessorios",
-      precoAntigo: 299.00,
-      precoNovo: 249.00,
-      img: "assets/images/fone3.webp",
-      descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas."
-    },
-    {
-      id: 19,
-      nome: "Fone Bluetooth",
-      categoria: "acessorios",
-      precoAntigo: 299.00,
-      precoNovo: 249.00,
-      img: "assets/images/fone4.webp",
-      descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas."
-    },
-    {
-      id: 20,
-      nome: "Fone Bluetooth",
-      categoria: "acessorios",
-      precoAntigo: 299.00,
-      precoNovo: 249.00,
-      img: "assets/images/fone5.webp",
-      descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas."
-    },
-  ];
+    const produtos = [
+  { id: 1, nome: "Camiseta Estampada", categoria: "roupas", precoAntigo: 79.90, precoNovo: 59.90, img: "assets/images/camisa2.png", descricao: "Camiseta 100% algodão com estampa moderna e confortável." },
+  { id: 2, nome: "Camiseta Estampada", categoria: "roupas", precoAntigo: 79.90, precoNovo: 59.90, img: "assets/images/camisa1.png", descricao: "Camiseta casual com design exclusivo e tecido respirável." },
+  { id: 3, nome: "Fone Bluetooth", categoria: "acessorios", precoAntigo: 299.00, precoNovo: 249.00, img: "assets/images/fone1.webp", descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas." },
+  { id: 4, nome: "iPhone 14 Pro", categoria: "celulares", precoAntigo: 7999.99, precoNovo: 5399.99, img: "assets/images/iph14pro2.webp", descricao: "Apple iPhone 14 Pro Max 256GB Prateado 6,7” 48MP" },
+  { id: 5, nome: "iPhone 16 Pro", categoria: "celulares", precoAntigo: 7999.99, precoNovo: 7399.99, img: "assets/images/iph16pro.webp", descricao: "iPhone 16 Pro Max Apple 256GB, Câmera Tripla de 48MP, Tela 6,9, Titânio-Deserto" },
+  { id: 6, nome: "PC Gamer", categoria: "informatica", precoAntigo: 2399.90, precoNovo: 1599.90, img: "assets/images/pc4.webp", descricao: "Completo Intel Core i5 SSD 480GB 16GB Teclado Mouse Mouse Pad e Headset Gamer Monitor 19 Windows 10 Pro " },
+  { id: 7, nome: "PC Gamer", categoria: "informatica", precoAntigo: 2699.90, precoNovo: 1899.90, img: "assets/images/pc5.webp", descricao: "PC Gamer Completo 3green Play Intel Core i7 16GB RAM Placa de vídeo Geforce 4GB SSD 480GB Monitor 20 75Hz Fonte 500W 3GP-056 " },
+  { id: 8, nome: "PC Gamer", categoria: "informatica", precoAntigo: 3699.90, precoNovo: 2899.90, img: "assets/images/pc6.webp", descricao: "PC Gamer AMD Ryzen 5 4600G, 16GB RAM, Placa de video Radeon Vega 7, SSD 480GB, Fonte 550W + Monitor 23 LED 75hz + Kit Gamer Completo " },
+  { id: 9, nome: "PC Gamer", categoria: "informatica", precoAntigo: 3699.90, precoNovo: 2899.90, img: "assets/images/pc7.webp", descricao: "PC Gamer Completo AMD Ryzen 7 5700G, Gráficos Radeon VEGA 8, 16GB DDR4, SSD 512GB, Fonte 500W, Monitor 21.5 75Hz, 3green Force - 3F-016 " },
+  { id: 10, nome: "PC Gamer", categoria: "informatica", precoAntigo: 4699.90, precoNovo: 3899.90, img: "assets/images/pc8.webp", descricao: "PC Gamer Completo Skill Ryzen 7 5700G, 16GB, Radeon Vega 8, SSD 512GB M.2, + Monitor 27 IPS 100Hz - SK27003 " },
+  { id: 11, nome: "iPhone 16 ", categoria: "celulares", precoAntigo: 7999.99, precoNovo: 6399.99, img: "assets/images/iph16.webp", descricao: "Apple iPhone 16 128GB Rosa 6,1 48MP iOS 5G" },
+  { id: 12, nome: "iPhone 15 ", categoria: "celulares", precoAntigo: 7999.99, precoNovo: 5399.99, img: "assets/images/iph15.webp", descricao: "Apple iPhone 15 de 256GB - Preto" },
+  { id: 13, nome: "Camiseta Estampada", categoria: "roupas", precoAntigo: 79.90, precoNovo: 59.90, img: "assets/images/camisa3.webp", descricao: "Camisa Social Masculina Manga Longa Lisa Slim - LORD FINESSE OUTLET." },
+  { id: 14, nome: "Camiseta Estampada", categoria: "roupas", precoAntigo: 79.90, precoNovo: 59.90, img: "assets/images/camisa4.webp", descricao: "Camisa Social Masculina Moda Maior Homens Plus Size - LORD FINESSE CAMISARIA" },
+  { id: 15, nome: "Camiseta do Campeão", categoria: "roupas", precoAntigo: 129.90, precoNovo: 99.90, img: "assets/images/camisa5.webp", descricao: "Camisa Grêmio I 21/22 Umbro Masculina - Azul+Branco" },
+  { id: 16, nome: "Camiseta do Campeão", categoria: "roupas", precoAntigo: 129.90, precoNovo: 99.90, img: "assets/images/camisa6.webp", descricao: "Camisa Grêmio I 25/26 Torcedor Umbro Feminina" },
+  { id: 17, nome: "Fone Bluetooth", categoria: "acessorios", precoAntigo: 299.00, precoNovo: 249.00, img: "assets/images/fone2.webp", descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas." },
+  { id: 18, nome: "Fone Bluetooth", categoria: "acessorios", precoAntigo: 299.00, precoNovo: 249.00, img: "assets/images/fone3.webp", descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas." },
+  { id: 19, nome: "Fone Bluetooth", categoria: "acessorios", precoAntigo: 299.00, precoNovo: 249.00, img: "assets/images/fone4.webp", descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas." },
+  { id: 20, nome: "Fone Bluetooth", categoria: "acessorios", precoAntigo: 299.00, precoNovo: 249.00, img: "assets/images/fone5.webp", descricao: "Fone sem fio com cancelamento de ruído e autonomia de até 20 horas." }
+];
 
   let carrinho = [];
   let produtoSelecionado = null;
